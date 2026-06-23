ab9cb0b70ac40a380ba145c9b810291d27d598b9
kth <pni2396@gmail.com>
2026-06-23 11:09:38 +0900

    first init;

---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module sparta_codekata {
+}
---
+++ src/sparta_codekata/Substract.java
@@ -0,0 +1,10 @@
+package sparta_codekata;
+
+public class Substract {
+	public static void main(String[] args) {
+	}
+	
+    public int solution(int num1, int num2) {
+        return num1-num2;
+    }
+}
